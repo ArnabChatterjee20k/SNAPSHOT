cf993a587daa9c4b72a68985d4b98a7e3ee062cb
arnab <arnabchatterjee.ac.2@gmail.com>
2022-12-24 11:16:21 +0530

    added port

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ if __name__ == '__main__':
     if not os.path.exists("/test.db"):
         with app.app_context():
             db.create_all()
-    app.run(debug=True,host="0.0.0.0")#for running on all devices in the network.
+    app.run(host="0.0.0.0",port=5000)#for running on all devices in the network.
